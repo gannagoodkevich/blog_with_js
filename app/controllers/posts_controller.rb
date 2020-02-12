27d1643be62ee6ea4cc8126340c7e194edb0ea46
gannagoodkevich <gannagoodkevich@gmail.com>
2020-02-12 14:57:26 +0300

    Add rubocop checker to github workflow

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,6 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-
-  def show
-
-  end
-
   def create
     @post = Post.create!(post_params)
     respond_to do |format|
