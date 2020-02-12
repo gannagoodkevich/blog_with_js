fa35eb70eb5acf80a1af07b3e4f46d22ea7d8984
gannagoodkevich <gannagoodkevich@gmail.com>
2020-02-12 14:53:43 +0300

    Create Post and Comment controllers and views, make addign new comments and posts with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+  def new
+    @post = Post.find_by(id: params[:post_id])
+    @comment = Comment.new
+  end
+
+  def index
+    @post = Post.find_by(id: params[:post_id])
+    @comments = @post.comments
+  end
+
+  def create
+    @post = Post.find_by(id: params[:post_id])
+    @comment = Comment.create!(comment_params)
+    @post.comments << @comment
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,29 @@
 class PostsController < ApplicationController
+  protect_from_forgery except: [:index]
+
+  def new
+    @post = Post.new
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+
+  def show
+
+  end
+
+  def create
+    @post = Post.create!(post_params)
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
